74f88837a064d83de07f7f853c0dfe34be4f7e6b
mralimalik <aliriazahmad72@gmail.com>
2024-05-09 09:06:42 +0100

    firebase integration.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:creekapp/controller/bookListing_controller.dart';
 import 'package:creekapp/controller/home_controller.dart';
 import 'package:creekapp/controller/sign_up_controller.dart';
 import 'package:creekapp/controller/user_controller.dart';
+import 'package:creekapp/firebase_options.dart';
 import 'package:creekapp/view/splash/splash_sceen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -11,7 +14,10 @@ import 'controller/login_auth_controller.dart';
 import 'controller/on_boarding_controller.dart';
 
 
-void main() {
+void main() async{
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+FirebaseFirestore.instance.settings=const Settings(persistenceEnabled: true,cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED);
   runApp(const MyApp());
   Get.put(OnBoardingController());
   Get.put(LoginAuthController());
